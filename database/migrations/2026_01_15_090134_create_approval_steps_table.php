f18f37525bdcf81c76360f502152c99007a74e05
www-data <canhuclong@gmail.com>
2026-05-03 16:08:04 +0700

    cap nhat cau truc

--- database/migrations/2026_01_15_090134_create_approval_steps_table.php
+++ database/migrations/2026_01_15_090134_create_approval_steps_table.php
@@ -28,6 +28,9 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('approval_steps');
+        Schema::enableForeignKeyConstraints();
+        
     }
 };
