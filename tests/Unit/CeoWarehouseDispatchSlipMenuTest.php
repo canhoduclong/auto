bd111537da9e819e79a58251efbc9eac27585129
www-data <canhuclong@gmail.com>
2026-08-28 11:25:44 +0700

    cap nhat cho layout giam doc

--- tests/Unit/CeoWarehouseDispatchSlipMenuTest.php
+++ tests/Unit/CeoWarehouseDispatchSlipMenuTest.php
@@ -8,14 +8,30 @@ use Tests\TestCase;
 
 class CeoWarehouseDispatchSlipMenuTest extends TestCase
 {
-    public function test_ceo_menu_groups_read_only_dispatch_slip_lookup_under_warehouse(): void
+    public function test_ceo_menu_groups_warehouse_overview_and_read_only_dispatch_slips_under_warehouse(): void
     {
         $layout = file_get_contents(__DIR__.'/../../resources/views/layouts/ceo.blade.php');
         $routes = file_get_contents(__DIR__.'/../../routes/web.php');
         $index = file_get_contents(__DIR__.'/../../resources/views/ceo/warehouse-dispatch-slips/index.blade.php');
 
         $this->assertStringContainsString('>Kho</div>', $layout);
+        $this->assertStringContainsString("route('ceo.warehouse')", $layout);
+        $this->assertStringContainsString('Tổng quan kho', $layout);
         $this->assertStringContainsString("route('ceo.warehouse-dispatch-slips.index')", $layout);
+        $this->assertStringContainsString('Phiếu xuất kho tổng', $layout);
+
+        $warehouseHeadingPosition = strpos($layout, '>Kho</div>');
+        $warehouseOverviewPosition = strpos($layout, "route('ceo.warehouse')", $warehouseHeadingPosition);
+        $dispatchSlipsPosition = strpos($layout, "route('ceo.warehouse-dispatch-slips.index')", $warehouseOverviewPosition);
+        $reportsHeadingPosition = strpos($layout, '>Báo Cáo</div>', $dispatchSlipsPosition);
+
+        $this->assertIsInt($warehouseHeadingPosition);
+        $this->assertIsInt($warehouseOverviewPosition);
+        $this->assertIsInt($dispatchSlipsPosition);
+        $this->assertIsInt($reportsHeadingPosition);
+        $this->assertLessThan($warehouseOverviewPosition, $warehouseHeadingPosition);
+        $this->assertLessThan($dispatchSlipsPosition, $warehouseOverviewPosition);
+        $this->assertLessThan($reportsHeadingPosition, $dispatchSlipsPosition);
         $this->assertStringContainsString("'ceoIndex'", $routes);
         $this->assertStringContainsString("'ceoShow'", $routes);
         $this->assertStringContainsString("'ceoPrintExport'", $routes);
